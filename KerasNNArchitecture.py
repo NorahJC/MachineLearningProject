601c2b42518de69e7ee29e06392922023c9acc5f
adrennan <30706272+adrennan@users.noreply.github.com>
2019-04-10 10:47:52 -0400

    Importing new version

--- KerasNNArchitecture.py
+++ KerasNNArchitecture.py
@@ -1,5 +1,4 @@
 import tensorflow as tf
-
 '''
 To DO - 
 Convert into python class with structure
@@ -19,15 +18,20 @@ class NeuralNetwork(object):
     num_hidden_layers = 0
     x_train, x_test, y_train, y_test = 0,0,0,0
     model_optimizer, model_loss = ' ', ' '
+    output_act_type = ""
+    output_nodes = 0
 
     # constructor
     def __init__(self, hidden_units=64, hiddent_act_type=tf.nn.relu, num_epochs=64, num_hidden_layers=6,
-                 model_optimizer='Nadam', model_loss='sparse_categorical_crossentropy'):
+                 model_optimizer='Nadam', model_loss='sparse_categorical_crossentropy',
+                 output_act_type=tf.nn.sigmoid, output_nodes = 100):
         self.hidden_units = hidden_units
         self.hidden_act_type = hiddent_act_type
         self.num_epochs = num_epochs
         self.num_hidden_layers = num_hidden_layers
         self.model_optimizer = model_optimizer
+        self.output_act_type = output_act_type
+        self.output_nodes = output_nodes
         self.model_loss = model_loss
 
     def load_data(self, X, y, train_size, test_size):
@@ -40,14 +44,17 @@ class NeuralNetwork(object):
         model = tf.keras.models.Sequential()
 
         # input layer
-        model.add(tf.keras.layers.Dense(32, input_shape=(21,)))
+        model.add(tf.keras.layers.Dense(32, input_shape=(19,)))
 
         # 3 hidden layers with relu activation
         for i in range(self.num_hidden_layers):
             model.add(tf.keras.layers.Dense(self.hidden_units, activation=self.hidden_act_type))
 
         # output layer (To DO - Change this to something better)
-        model.add(tf.keras.layers.Dense(4, activation=tf.nn.elu))
+        model.add(tf.keras.layers.Dense(self.output_nodes, activation=self.output_act_type))
+        # soft plus max test = .51
+        # softmax = .51
+        # sigmoid = .51
 
         '''
         https://keras.io/activations/
@@ -57,10 +64,12 @@ class NeuralNetwork(object):
         '''
         model.compile(optimizer=self.model_optimizer,
                       loss=self.model_loss,
-                      metrics=['accuracy']
-                      )
-        model.fit(self.x_test, self.y_test, epochs=self.num_epochs)
+                      metrics=['accuracy'])
 
-        loss, acc = model.evaluate(self.x_train, self.y_train)
+        print(self.x_train, self.y_train)
+        model.fit(self.x_train, self.y_train, epochs=self.num_epochs)
+
+
+        loss, acc = model.evaluate(self.x_test, self.y_test)
         print('test set loss: {} \ntest set accuracy {}'.format(loss, acc))
         return loss, acc, model
--- imagedataNN.py
+++ imagedataNN.py
@@ -1,46 +1,70 @@
 from KerasNNArchitecture import NeuralNetwork
 import pandas as pd, numpy as np, tensorflow as tf
+from sklearn.preprocessing import StandardScaler
 
-'''
-def __init__(self, hidden_units=64, hiddent_act_type=tf.nn.relu, num_epochs=64, num_hidden_layers=6,
-                 model_optimizer='Nadam', model_loss='sparse_categorical_crossentropy'):
-'''
 test = NeuralNetwork()
 
-bio_data = pd.read_csv('C:/Users/Alex/Documents/School/Machine Learning/Project/output_nn/61016P1D220.csv')
+
+bio_data = pd.read_csv('C:/Users/Alex/Documents/School/Machine Learning/Project/output_nn/61016P2D221.csv')
+
+'''
+Try loading all the csv data instead of just one person
+
+Training set mix of  
+'''
 bio_scores = bio_data.iloc[:, :].values
 count = 0
 for i in range(len(bio_scores)):
     if not pd.isnull(bio_scores[i][22]):
         count = count + 1
 
-X = np.ones((count, 21), dtype=float)
-y = np.ones((count, 1), dtype=float)
+X = np.ones((count, 19), dtype=float)
+y = np.ones((count, 3, 1), dtype=float)
 offset = 0
 for i in range(len(bio_scores)):
-
     if not pd.isnull(bio_scores[i][22]):
-        for j in range(21):
-            X[i - offset][j] = bio_scores[i][j]
-        y[i - offset][0] = bio_scores[i][21] / 100
+        for j in range(1, 20):
+            X[i - offset][j - 1] = bio_scores[i][j]
+
+        y[i - offset][0][0] = bio_scores[i][21] / 100
+        y[i - offset][1][0] = bio_scores[i][22] / 100
+        y[i - offset][2][0] = bio_scores[i][23] / 100  #dividing by 100 (our scale) gives higher accuracies
     else:
         offset = offset + 1
-print(np.shape(y))
-print(np.shape(X))
-print(y[402])
+
+'''
+scaler = StandardScaler()
+print(scaler.fit(X))
+StandardScaler(copy=True, with_mean=True, with_std=True)
+X = scaler.transform(X[:][:])
+'''
+
 acc = 0
-test.num_epochs = 16
-test.num_hidden_layers = 16
-test.hidden_units = 20
-test.load_data(X, y, 300, 100)
+test.num_epochs = 1
+test.num_hidden_layers = 20
+test.hidden_units = 100
+trainSize = int(len(X) * .8)
+test.load_data(X, y, trainSize, len(X) - trainSize)
+test.hidden_act_type= tf.nn.leaky_relu
+test.output_nodes = 3
+test.output_act_type= tf.nn.softmax
 count = 0
-while acc < .95:
+prevAcc = -1
+maxAcc = -1
+bestEpochNum = 0
+
+while acc < .95 and count < 10:
+    prevAcc = acc
     loss, acc, model = test.evaluate()
-    test.hidden_units = test.hidden_units + 1
-    listTest = np.sum(model.predict(X[302:402]), axis=1).tolist()
-    print(model.predict(X[302:402]))
-    print(listTest)
+    test.num_epochs = test.num_epochs + 1
+    print(model.predict(X[len(X) - trainSize:]))
+
+    #print("List of predictions on test set: \n{}".format(listTest))
+    print("List of y values: \n{}".format(np.transpose(y[len(y) - trainSize:])))
     print(acc)
     count = count + 1
-print(count)
-
+    print(count)
+    if acc > maxAcc:
+        maxAcc = acc
+        bestEpochNum = test.num_epochs
+print("Best accuracy found to be {} with epochs num of {}".format(maxAcc, bestEpochNum))
